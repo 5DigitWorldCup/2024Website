569302b4157e5b1d4fa802b10bca0caa642e9f80
Mario564 <ka.mario564@gmail.com>
2023-12-07 14:26:41 -0600

    CORS issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import BottomLandingBar from '@/components/Landing/Bars/BottomLandingBar';
 import TopLandingBar from '@/components/Landing/Bars/TopLandingBar';
 import LandingLogo from '@/components/Landing/Logo/LandingLogo';
 import styles from './page.module.scss';
+import { buildApiUrl } from '@/utils';
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const resp = await fetch(buildApiUrl('/auth/session'));
+
+  if (resp.ok) {
+    console.log(await resp.json())
+  }
+
   return (
     <div className={styles.container}>
       <TopLandingBar />
