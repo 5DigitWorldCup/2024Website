db94a96f1734978b607e758b7caaa0bb5023f108
Mario564 <ka.mario564@gmail.com>
2023-12-16 21:05:29 +0000

    Implement auth flow

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,8 @@ import BottomLandingBar from '@/components/Landing/Bars/BottomLandingBar';
 import TopLandingBar from '@/components/Landing/Bars/TopLandingBar';
 import LandingLogo from '@/components/Landing/Logo/LandingLogo';
 import styles from './page.module.scss';
-import { buildApiUrl } from '@/utils';
 
-export default async function LandingPage() {
-  const resp = await fetch(buildApiUrl('/auth/session'));
-
-  if (resp.ok) {
-    console.log(await resp.json())
-  }
-
+export default function LandingPage() {
   return (
     <div className={styles.container}>
       <TopLandingBar />
